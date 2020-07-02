0fe9bb84d98916a603a12d66483f4996188abcb9
w58296 <582961331@qq.com>
2020-07-02 08:42:04 +0800

    老人义工交互的初步实现  Version 1.0

--- vision/facedetection/testingfacerecognition.py
+++ vision/facedetection/testingfacerecognition.py
@@ -13,6 +13,7 @@ import imutils
 import cv2
 import time
 import argparse
+import math
 
 # 传入参数
 ap = argparse.ArgumentParser()
@@ -23,6 +24,16 @@ args = vars(ap.parse_args())
 # 全局变量
 facial_recognition_model_path = 'models/face_recognition_hog.pickle'
 input_video = args['filename']
+
+old_name = '106'
+work_name = '103'
+
+def calculate_distance(p1, p2):
+    x = p1[0] - p2[0]
+    y = p1[1] - p2[1]
+    return math.sqrt(x*x+y*y)
+
+
 
 # 初始化摄像头
 if not input_video:
@@ -54,6 +65,17 @@ while True:
     face_location_list, names = faceutil.get_face_location_and_name(
         frame)
 
+    interaction = False
+
+    if work_name in names and old_name in names:
+        interaction = True
+
+    points = []
+
+    point_size = 5
+    point_color = (0, 0, 255)
+    thickness = 4
+
     # loop over the face bounding boxes
     for ((left, top, right, bottom), name) in zip(
             face_location_list,
@@ -63,6 +85,21 @@ while True:
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         cv2.rectangle(frame, (left, top), (right, bottom),
                       (0, 0, 255), 2)
+
+
+        if old_name == name or work_name == name:
+            points.append((int((left+right)/2), int((top+bottom)/2)))
+
+        if interaction:
+            cv2.circle(frame, (int((left+right)/2), int((top-bottom)/2)), point_size, color=point_color, thickness=thickness)
+            #print("interaction process")
+
+    if interaction:
+        distance = calculate_distance(points[0], points[1])
+
+        if distance < 100:
+            cv2.line(frame, points[0], points[1], color=point_color, thickness=thickness)
+            cv2.putText(frame, 'distance %d cm' % distance, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     # show our detected faces along with smiling/not smiling labels
     cv2.imshow("Face Recognition", frame)
